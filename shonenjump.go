2406b27666daea06a6a894b3b591757e8b6b720c
satoru <satorulogic@gmail.com>
2016-05-04 12:28:27 +0800

    Implement saving entries to file

--- shonenjump.go
+++ shonenjump.go
@@ -4,12 +4,35 @@ import (
     "fmt"
     "os"
     "bufio"
+    "io/ioutil"
     "strings"
     "log"
     "strconv"
     "sort"
     "math"
+    "bytes"
 )
+
+func saveEntries(entries []*Entry, path string) {
+    tempfile, err := ioutil.TempFile("", "")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer os.Remove(tempfile.Name())
+
+    var buffer bytes.Buffer
+    for _, e := range entries {
+        buffer.WriteString(e.String() + "\n")
+    }
+    if _, err := tempfile.Write(buffer.Bytes()); err != nil {
+        log.Fatal(err)
+    }
+    if err := tempfile.Close(); err != nil {
+        log.Fatal(err)
+    }
+
+    os.Rename(tempfile.Name(), path)
+}
 
 func updateEntriesWithPath(entries []*Entry, path string, weight float64) []*Entry {
     path = strings.TrimSuffix(path, string(os.PathSeparator))
@@ -40,6 +63,10 @@ type Entry struct {
 func (e *Entry) updateScore(weight float64) float64 {
     e.Score = math.Sqrt(math.Pow(e.Score, 2) + math.Pow(weight, 2))
     return e.Score
+}
+
+func (e Entry) String() string {
+    return fmt.Sprintf("%f\t%s", e.Score, e.Path)
 }
 
 type ByScore []*Entry
@@ -98,7 +125,7 @@ func main() {
     weight := 10.0
 
     entries = updateEntriesWithPath(entries, path, weight)
-    for _, e := range entries {
-        fmt.Println(e.Score, e.Path)
-    }
+
+    destPath := "/tmp/shonenjump.txt"
+    saveEntries(entries, destPath)
 }
