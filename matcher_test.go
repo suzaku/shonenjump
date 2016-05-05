6709d71b41d809c4197aaee340ca6e3627ee9f92
satoru <satorulogic@gmail.com>
2016-05-05 11:32:22 +0800

    Add tests for the other two matchers

--- matcher_test.go
+++ matcher_test.go
@@ -2,6 +2,37 @@ package main
 
 import "testing"
 
+func TestAnywhere(t *testing.T) {
+    entries := []*Entry{
+        &Entry{"/foo/bar/baz", 10},
+        &Entry{"/foo/bazar", 10},
+        &Entry{"/tmp", 10},
+        &Entry{"/foo/gxxbazabc", 10},
+    }
+    result := matchAnywhere(entries, []string{"foo", "baz"})
+    expected := []string{
+        "/foo/bar/baz",
+        "/foo/bazar",
+        "/foo/gxxbazabc",
+    }
+    assertItemsEqual(t, result, expected)
+}
+
+func TestFuzzy(t *testing.T) {
+    entries := []*Entry{
+        &Entry{"/foo/bar/baz", 10},
+        &Entry{"/foo/bazar", 10},
+        &Entry{"/tmp", 10},
+        &Entry{"/foo/gxxbazabc", 10},
+    }
+    result := matchFuzzy(entries, []string{"baz"})
+    expected := []string{
+        "/foo/bar/baz",
+        "/foo/bazar",
+    }
+    assertItemsEqual(t, result, expected)
+}
+
 func TestConsecutive(t *testing.T) {
     entries := []*Entry{
         &Entry{"/foo/bar/baz", 10},
@@ -16,6 +47,10 @@ func TestConsecutive(t *testing.T) {
         "/foo/bazar",
         "/foo/xxbaz",
     }
+    assertItemsEqual(t, result, expected)
+}
+
+func assertItemsEqual(t *testing.T, result []string, expected []string) {
     if len(result) != len(expected) {
         t.Errorf("Expected %v, got %v", expected, result)
     }
