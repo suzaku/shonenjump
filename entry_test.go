6041e346c18e28dbdb167ba87df6b90961965f4b
satoru <satorulogic@gmail.com>
2016-05-06 16:23:15 +0800

    Add test for updateScore

--- entry_test.go
+++ entry_test.go
@@ -1,27 +1,39 @@
 package main
 
 import (
-    "testing"
-    "strings"
+	"strings"
+	"testing"
 )
 
+func TestUpdateEntryScore(t *testing.T) {
+	e := &Entry{"/etc/init", 0}
+	e.updateScore(10)
+	if e.Score != 10 {
+		t.Errorf("Entity score is wrong: %f", e.Score)
+	}
+	e.updateScore(10)
+	if e.Score-14.14 < 0.001 {
+		t.Errorf("Entity score is wrong: %f", e.Score)
+	}
+}
+
 func TestClearNotExistDirs(t *testing.T) {
-    orig := isValidPath
-    defer func() { isValidPath = orig }()
-    isValidPath = func(p string) bool {
-        return !strings.HasSuffix(p, "not-exist")
-    }
-    entries := []*Entry{
-        &Entry{"/foo/bar", 10},
-        &Entry{"/foo/not-exist", 10},
-        &Entry{"/tmp", 10},
-        &Entry{"/not-exist", 10},
-    }
-    result := clearNotExistDirs(entries)
-    var output []string
-    for _, r := range result {
-        output = append(output, r.Path)
-    }
-    expected := []string{"/foo/bar", "/tmp"}
-    assertItemsEqual(t, output, expected)
+	orig := isValidPath
+	defer func() { isValidPath = orig }()
+	isValidPath = func(p string) bool {
+		return !strings.HasSuffix(p, "not-exist")
+	}
+	entries := []*Entry{
+		&Entry{"/foo/bar", 10},
+		&Entry{"/foo/not-exist", 10},
+		&Entry{"/tmp", 10},
+		&Entry{"/not-exist", 10},
+	}
+	result := clearNotExistDirs(entries)
+	var output []string
+	for _, r := range result {
+		output = append(output, r.Path)
+	}
+	expected := []string{"/foo/bar", "/tmp"}
+	assertItemsEqual(t, output, expected)
 }
